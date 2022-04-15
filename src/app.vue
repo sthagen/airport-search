e03b6ceb50dca3c32fc7d0039f5dce5d18e7987f
Stefan Hagen <stefan@hagen.link>
2022-04-15 14:46:35 +0200

    Refactored file system interface

--- src/app.vue
+++ src/app.vue
@@ -1,10 +1,10 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld msg="Airport Search" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "./components/hello_world.vue";
 
 export default {
   name: "App",
