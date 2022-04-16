9e780a2e1b651feef2f177dbba1d1cddef91524a
Stefan Hagen <stefan@hagen.link>
2022-04-16 18:54:24 +0200

    Added box shadow on hover to action button

--- src/components/action_button.vue
+++ src/components/action_button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="px-5 py-3 font-medium text-white border-0 rounded bg-brand-green-1">Action</button>
+  <button class="px-5 py-3 font-medium text-white border-0 rounded bg-brand-green-1 hover:shadow-green">Action</button>
 </template>
 
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         'brand-blue-1': '#1967d2',
         'brand-green-1': '#137333',
       },
+      boxShadow: {
+        green: '0 0 3px 3px #42f485',
+      },
     },
   },
   variants: {
