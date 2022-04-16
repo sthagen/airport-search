7d169229d54cfc61a7d5f5df4f801420ab332634
Stefan Hagen <stefan@hagen.link>
2022-04-16 18:49:15 +0200

    Added action button

---
+++ src/components/action_button.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="px-5 py-3 font-medium text-white border-0 rounded bg-brand-green-1">Action</button>
+</template>
+
+<script>
+export default {
+  name: 'ActionButton',
+}
+</script>
